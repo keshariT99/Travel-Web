7a475270d99e77ebc46ba4ad5d25d3a71ee6d6fb
KTennakoon <110615349+keshariT99@users.noreply.github.com>
2024-12-22 14:11:02 +0530

    Create Gallery features

---
+++ src/pages/Gallery.jsx
@@ -0,0 +1,125 @@
+import React from 'react'
+
+const Gallery = () => {
+  return (
+    <div>
+      <h3 className='text-3xl font-bold text-center mt-6 pt-20'>Gallary</h3>
+      <div className="border-t border-gray-800 mt-6 pt-2 text-center text-gray-400"></div>
+
+      {/*Colombo gallery */}
+      <div className='py12 bg-white-100 m-8'>
+        <div className='bg-white py-8 px-4 rounded-lg shadow-lg'>
+      
+          <div className='text-center mb-8'>
+            <h2 className='text-3xl md:text-4xl font-bold text-gray-800'>
+              Recommended places around <span className='text-orange-500'>the Colombo.</span>
+            </h2>
+            <p className='text-md md:text-lg text-gray-600 mt-2'>
+              Most visited destinations of the year
+            </p>
+          </div>
+
+          {/* colombo Gallery Section */}
+          <div className='flex justify-center items-center gap-4'>
+            <img className='w-40 h-64 sm:w-36 sm:h-52 md:w-36 md:h-54 object-cover rounded-lg shadow-lg' src='/Images/Gallery/colombo7.jpg' />
+
+            <img className='w-48 h-72 sm:w-40 sm:h-60 md:w-44 md:h-66 object-cover rounded-lg shadow-lg' src='/Images/Gallery/colombo6.jpg' />
+
+            <img className='w-56 h-80 sm:w-48 sm:h-72 md:w-52 md:h-76 object-cover rounded-lg shadow-lg' src='/Images/Gallery/colombo2.jpg' />
+
+            <img className='w-56 h-80 sm:w-56 sm:h-80 md:w-60 md:h-96 object-cover rounded-lg shadow-lg' src='/Images/colombo.jpg' />
+
+            <img className='w-56 h-80 sm:w-48 sm:h-72 md:w-52 md:h-76 object-cover rounded-lg shadow-lg' src='/Images/Gallery/colombo3.jpg' />
+          
+            <img className='w-48 h-72 sm:w-40 sm:h-60 md:w-44 md:h-66 object-cover rounded-lg shadow-lg' src='/Images/Gallery/colombo4.jpg' />
+
+            <img className='w-40 h-64 sm:w-36 sm:h-52 md:w-36 md:h-54 object-cover rounded-lg shadow-lg' src='/Images/Gallery/colombo1.jpg' />
+
+          </div>
+        
+        </div>
+      
+      </div>
+
+      {/*Sigiriya gallery */}
+
+      <div className='py12 bg-white-100 m-8'>
+        <div className='bg-white py-8 px-4 rounded-lg shadow-lg'>
+      
+          <div className='text-center mb-8'>
+            <h2 className='text-3xl md:text-4xl font-bold text-gray-800'>
+              Recommended places around <span className='text-orange-500'>the Matale.</span>
+            </h2>
+            <p className='text-md md:text-lg text-gray-600 mt-2'>
+              Most visited destinations of the year
+            </p>
+          </div>
+
+          {/* Gallery Section */}
+          <div className='flex justify-center items-center gap-4'>
+            <img className='w-40 h-64 sm:w-36 sm:h-52 md:w-36 md:h-54 object-cover rounded-lg shadow-lg' src='/Images/Gallery/matale1.jpg' />
+
+            <img className='w-48 h-72 sm:w-40 sm:h-60 md:w-44 md:h-66 object-cover rounded-lg shadow-lg' src='/Images/Gallery/matale2.jpg' />
+
+            <img className='w-56 h-80 sm:w-48 sm:h-72 md:w-52 md:h-76 object-cover rounded-lg shadow-lg' src='/Images/Gallery/matale3.jpg' />
+
+            <img className='w-56 h-80 sm:w-56 sm:h-80 md:w-60 md:h-96 object-cover rounded-lg shadow-lg' src='/Images/Gallery/matalee4.jpg' />
+
+            <img className='w-56 h-80 sm:w-48 sm:h-72 md:w-52 md:h-76 object-cover rounded-lg shadow-lg' src='/Images/Gallery/matale5.jpg' />
+          
+            <img className='w-48 h-72 sm:w-40 sm:h-60 md:w-44 md:h-66 object-cover rounded-lg shadow-lg' src='/Images/Gallery/matale6.jpg' />
+
+            <img className='w-40 h-64 sm:w-36 sm:h-52 md:w-36 md:h-54 object-cover rounded-lg shadow-lg' src='/Images/Gallery/matale7.jpg' />
+
+          </div>
+        
+        </div>
+      
+      </div>
+
+      {/*Kandy */}
+
+      <div className='py12 bg-white-100 m-8'>
+        <div className='bg-white py-8 px-4 rounded-lg shadow-lg'>
+      
+          <div className='text-center mb-8'>
+            <h2 className='text-3xl md:text-4xl font-bold text-gray-800'>
+              Recommended places around <span className='text-orange-500'>the Kandy.</span>
+            </h2>
+            <p className='text-md md:text-lg text-gray-600 mt-2'>
+              Most visited destinations of the year
+            </p>
+          </div>
+
+          {/* Gallery Section */}
+          <div className='flex justify-center items-center gap-4 '>
+            <img className='w-40 h-64 sm:w-36 sm:h-52 md:w-36 md:h-54 object-cover rounded-lg shadow-lg cursor-pointer
+                    transform transition duration-300 hover:scale-110' 
+            src='/Images/Gallery/kandy6.jpg' />
+
+            <img className='w-48 h-72 sm:w-40 sm:h-60 md:w-44 md:h-66 object-cover rounded-lg shadow-lg cursor-pointer
+                    transform transition duration-300 hover:scale-110' 
+            src='/Images/Gallery/kandy1.jpg' />
+
+            <img className='w-56 h-80 sm:w-48 sm:h-72 md:w-52 md:h-76 object-cover rounded-lg shadow-lg cursor-pointer
+                    transform transition duration-300 hover:scale-110' 
+            src='/Images/Gallery/kandy.jpg' />
+
+            <img className='w-56 h-80 sm:w-56 sm:h-80 md:w-60 md:h-96 object-cover rounded-lg shadow-lg' src='/Images/Gallery/kandy4.jpg' />
+
+            <img className='w-56 h-80 sm:w-48 sm:h-72 md:w-52 md:h-76 object-cover rounded-lg shadow-lg' src='/Images/Gallery/kandy7.jpg' />
+          
+            <img className='w-48 h-72 sm:w-40 sm:h-60 md:w-44 md:h-66 object-cover rounded-lg shadow-lg' src='/Images/Gallery/kandy5.jpg' />
+
+            <img className='w-40 h-64 sm:w-36 sm:h-52 md:w-36 md:h-54 object-cover rounded-lg shadow-lg' src='/Images/Gallery/kandy2.jpg' />
+
+          </div>
+        
+        </div>
+      
+      </div>
+    </div>
+  )
+}
+
+export default Gallery
